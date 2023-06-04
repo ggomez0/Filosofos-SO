4b520cd046f6a3f40a28840e4c15dfd08df52c94
ggomez0 <gaspargomez2000@outlook.com>
2023-06-04 14:50:10 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FilosofosSO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
